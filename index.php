b7b9cf8d35e13355d6b78792fadf8804be26cc0c
Marco Constancio <Marco Constancio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-16 15:21:20 +0000

    Fixed security issue in the thumbnail generation script.
    git-svn-id: https://plugins.svn.wordpress.org/mediarss-external-gallery/trunk@439125 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
-<?php 
+<?php
 /*
 Plugin Name: MediaRSS external gallery
-Plugin URI: 
-Description: Generates a thumbnails gallery from a media rss feed url. 
-Version: 0.1
+Plugin URI:
+Description: Generates a thumbnails gallery from a media rss feed url.
+Version: 0.2
 Author: Marco Constâncio
 Author URI: http://www.betasix.net
 */
 
 
 function generate_meg_gallery($meg_param){
-	
+
 $all_content ="No Media Rss url set.<br>";
 
 if(isset($meg_param['url'])){
@@ -21,55 +21,55 @@ if(isset($meg_param['url'])){
 	$feed->set_cache_location('./wp-content/cache');
 	$feed->enable_cache(true);
 	$feed->init();
-	
+
 	$items = $feed->get_items(0,$meg_param["max_items"]);
-	
+
 	$all_content ="";
 	$classes = array("img"=>"","item"=>"");
 	//DEFAUTL VALUES
 	if(!isset($meg_param["columns"])){ $meg_param["columns"] = 3; }
-	
-	if(isset($meg_param["items"])){ 
-		$meg_param["items"] = explode(",",$meg_param["items"]); 
-	}else{ 
-		$meg_param["items"] = array("title_feed_link","image_file_link"); 
-	} 
+
+	if(isset($meg_param["items"])){
+		$meg_param["items"] = explode(",",$meg_param["items"]);
+	}else{
+		$meg_param["items"] = array("title_feed_link","image_file_link");
+	}
 
 	if(isset($meg_param["image_border"])){ $classes["img"] = " meg_border"; }
 	if(isset($meg_param["item_border"])){ $classes["item"] = " meg_border"; }
-	
+
 	$meg_param["img_src_param"] = "";
 	$meg_param["img_src"] = "";
-	 	 
+
 	//IMAGE CACHE RESIZE AND CACHE
-	if(isset($meg_param["width"]) || isset($meg_param["height"])){ 
-		//ADD RESQUESTED DOMAIN TO THE ALLOWED SITES CONFIG FILE IF NECESSARY 
+	if(isset($meg_param["width"]) || isset($meg_param["height"])){
+		//ADD RESQUESTED DOMAIN TO THE ALLOWED SITES CONFIG FILE IF NECESSARY
 		$allowedSites = read_config_file();
 		$feed_domain = get_domain($meg_param["url"]);
 		if(!in_array($feed_domain,$allowedSites)){
 			$allowedSites[]=$feed_domain;
 			write_config_file($allowedSites);
 		}
-		
+
 		$meg_param["img_src"].= home_url()."/wp-content/plugins/mediarss-external-gallery/timthumb.php?src=";
-		if(isset($meg_param["width"])){ 
+		if(isset($meg_param["width"])){
 			$meg_param["img_src_param"].="&w=".$meg_param["width"];
 		}
-		if(isset($meg_param["height"])){ 
+		if(isset($meg_param["height"])){
 			$meg_param["img_src_param"].="&h=".$meg_param["height"];
-		}	
+		}
 	}
 
 	$i=0;
-	
+
 	$content = array();
 	$line = array();
 	foreach ($items as $item){
 		$item_data ="";
 		foreach($meg_param["items"] as $item_type){
-				$item_data.= get_feed_data($item->get_enclosure(),$item,$meg_param,$item_type,$classes)."<br>";			
+				$item_data.= get_feed_data($item->get_enclosure(),$item,$meg_param,$item_type,$classes)."<br>";
 		}
-	
+
 		$line[] = "<div class='meg_item ".$classes["item"]."'>".$item_data."</div>";
 
 		$i++;
@@ -79,10 +79,10 @@ if(isset($meg_param['url'])){
 			$i=0;
 		}
 	}
-	
+
 	if($i!=0){ $content[] = $line; }
 
-	
+
 	$all_content.="<div class='meg_table'>";
 	foreach($content as $line){
 		$all_content.= "<div class='meg_line'>";
@@ -90,21 +90,21 @@ if(isset($meg_param['url'])){
 		$all_content.= "<div class='clear'></div></div>";
 	}
 	$all_content.="</div>";
-	
-	
+
+
 	$all_content.="<script language='javascript' type='text/javascript' defer>
 					<!--
-					jQuery('.meg_line').each(function() {							
+					jQuery('.meg_line').each(function() {
 						var tallest_height = 0;
-						var current_height = 0;			
-								
-						jQuery(this).find('.meg_cell .meg_item').each(function() {	
+						var current_height = 0;
+
+						jQuery(this).find('.meg_cell .meg_item').each(function() {
 							current_height = jQuery(this).height();
 							if(current_height > tallest_height){
 								tallest_height=current_height ;
 							}
 						});
-						
+
 						jQuery(this).find('.meg_cell .meg_item').each(function() {
 							jQuery(this).height(tallest_height);
 						});
@@ -112,13 +112,13 @@ if(isset($meg_param['url'])){
 
 					// -->
 					</script>";
-	
-	return $all_content; 
+
+	return $all_content;
 	}
 }
 
 #GETS A SPECIFIC FEED DATA
-function get_feed_data($enclosure,$item,$meg_param,$option,$classes){	
+function get_feed_data($enclosure,$item,$meg_param,$option,$classes){
 	switch ($option) {
 		case "image":
 			return "<img src='".$meg_param["img_src"].$enclosure->get_thumbnail().$meg_param["img_src_param"]."' alt='".$enclosure->get_title()."' class='wp-post-img meg-img ".$classes["img"]."' />&nbsp;";
@@ -135,20 +135,20 @@ function get_feed_data($enclosure,$item,$meg_param,$option,$classes){
 			return "<a href='".$enclosure->get_link()."'>".SimplePie_Misc::htmlspecialchars_decode($enclosure->get_title(),ENT_COMPAT)."</a>&nbsp;";
 		case "title_feed_link":
 			return "<a href='".$item->get_permalink()."'>".SimplePie_Misc::htmlspecialchars_decode($enclosure->get_title(),ENT_COMPAT)."</a>&nbsp;";
-							
+
 		case "description":
-			return strip_tags(SimplePie_Misc::htmlspecialchars_decode($enclosure->get_description(),ENT_COMPAT))."&nbsp;";	
+			return strip_tags(SimplePie_Misc::htmlspecialchars_decode($enclosure->get_description(),ENT_COMPAT))."&nbsp;";
 		case "description_file_link":
 			return "<a href='".$enclosure->get_link()."'>".strip_tags(SimplePie_Misc::htmlspecialchars_decode($enclosure->get_description(),ENT_COMPAT))."</a>&nbsp;";
 		case "description_feed_link":
-			return "<a href='".$item->get_permalink()."'>".strip_tags(SimplePie_Misc::htmlspecialchars_decode($enclosure->get_description(),ENT_COMPAT))."</a>&nbsp;";	      
+			return "<a href='".$item->get_permalink()."'>".strip_tags(SimplePie_Misc::htmlspecialchars_decode($enclosure->get_description(),ENT_COMPAT))."</a>&nbsp;";
 	}
 }
 
 #EXTRACT DOMAIN FROM A FULL URL
 function get_domain($full_url=""){
 	preg_match("/^(http:\/\/)?([^\/]+)/i", $full_url, $domain_only);
-	return $domain_only[2]; 
+	return $domain_only[2];
 }
 
 #READS THE LIST OF ALLOWED SITES CONFIG FILE
--- readme.txt
+++ readme.txt
@@ -1,23 +1,23 @@
 === MediaRSS external gallery ===
 Contributors: Marco Constâncio
-Tags: rss, feed, media, gallery 
+Tags: rss, feed, media, gallery
 Requires at least: 3.1
-Tested up to: 3.1.1
+Tested up to: 3.2.1
 Stable Tag: trunk
 
-Allows the user to insert a gallery on page/post from media rss feed url using shortcodes. 
+Allows the user to insert a gallery on page/post from media rss feed url using shortcodes.
 
 == Description ==
 
-Allows the user to insert a gallery on page/post from media rss feed url using shortcodes. 
-It was mainly created to generate image galleries, but it can also be used to create galleries for other file types feed. 
+Allows the user to insert a gallery on page/post from media rss feed url using shortcodes.
+It was mainly created to generate image galleries, but it can also be used to create galleries for other file types feed.
 
-Some of the functions include generation of links to the feed/file, thumbnail creation and image caching. 
+Some of the functions include generation of links to the feed/file, thumbnail creation and image caching.
 
 == Installation ==
 
-1. Install the plugin either by uploading the contents of 
-mediarss-external-gallery.zip to the '/wp-content/plugins/' directory 
+1. Install the plugin either by uploading the contents of
+mediarss-external-gallery.zip to the '/wp-content/plugins/' directory
 or by using the 'Add New' in 'Plugins' menu in WordPress
 1. Activate the plugin through the 'Plugins' menu in WordPress
 1. Create the directory '/wp-content/cache' if does not exist
@@ -25,7 +25,7 @@ or by using the 'Add New' in 'Plugins' menu in WordPress
 = Parameters =
 * **url** - url fo the feed. (mandatory)
 * **columns** - number of columns of item for each line (default: 3)
-* **width** or **height** - size in pixels to be used in image resizing. If only one option is set the other option will automatically determined. This uses image caching wich requires the */wp-content/cache* folder to exist.   
+* **width** or **height** - size in pixels to be used in image resizing. If only one option is set the other option will automatically determined. This uses image caching wich requires the */wp-content/cache* folder to exist.
 * **image_border** - border around the image
 * **item_border** - border around the entire item
 * **items** - comma separated items to be included in each cell wich can be: *image, *image_thickbox*, *image_file_link*, *image_feed_link*, *title*, *title_file_link*, *title_feed_link*, *description*, *description_file_link*, *description_feed_link*
@@ -46,3 +46,5 @@ or by using the 'Add New' in 'Plugins' menu in WordPress
 = 0.1 =
 First version of the plugin.
 
+= 0.2 =
+Fixed security issue in the thumbnail generation script.
